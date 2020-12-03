d51ab2bbeacd3b3a1ca8a9c15b4177d919ad3bcb
RishabhGarg108 <rishabhgarg108@gmail.com>
2020-12-03 13:32:12 +0530

    Updated Competitions

--- frontend/src/components/Competitions.js
+++ frontend/src/components/Competitions.js
@@ -1,33 +1,117 @@
 import React, { Component } from "react";
 import "./Competitions.css";
+import { COMPETITIONS } from "../assets/CompetitionsDetail";
 
 class Competitions extends Component {
   constructor(props) {
     super(props);
-    this.state = { competition_type: null };
+    this.state = { competition_type: null, show_rank: null };
   }
 
-  handleMouseOver(e) {
-    console.log(e.type);
+  handleMouseOver(competition) {
+    this.setState({ show_rank: competition });
   }
 
-  handleMouseLeave(e) {
-    console.log(e.type);
+  handleMouseLeave() {
+    this.setState({ show_rank: null });
   }
 
   handleClick(event) {
     const card = event.target;
-    const card_body = card.childNodes[1];
-    const card_title = card_body.childNodes[0];
+    const card_title = card.childNodes[1].childNodes[0];
     const competition_type = card_title.innerHTML;
-    if (this.state.competition_type != competition_type) {
+    if (this.state.competition_type !== competition_type) {
       this.setState({ competition_type });
     } else {
       this.setState({ competition_type: null });
     }
   }
 
+  renderCompetition(competition) {
+    return (
+      <div
+        className="card flex-fill"
+        onMouseOver={(e) => {
+          this.handleMouseOver(competition);
+        }}
+        onMouseLeave={(e) => {
+          this.handleMouseLeave();
+        }}
+      >
+        {this.state.show_rank !== competition ? (
+          <img src="#" className="card-img-top" alt="graphic design"></img>
+        ) : null}
+
+        {this.state.show_rank !== competition ? (
+          <div className="card-body">
+            <h5 className="card-title">{competition.name}</h5>
+            <p className="card-text">{competition.date}</p>
+          </div>
+        ) : (
+          <div className="card-body" id="ranking_list">
+            <ul className="card-text">
+              <li>#1: {competition.rankings.first}</li>
+              <li>#2: {competition.rankings.second}</li>
+              <li>#3: {competition.rankings.third}</li>
+            </ul>
+          </div>
+        )}
+      </div>
+    );
+  }
+
+  renderPastCompetitions(competitions) {
+    let past_competitions = [...competitions];
+    past_competitions = past_competitions.map((elem, idx) => {
+      return (
+        <div className="col-12 col-sm-6 col-md-3 d-flex" key={idx}>
+          {this.renderCompetition(elem)}
+        </div>
+      );
+    });
+    console.log(past_competitions);
+
+    return (
+      <div className="row">
+        <div className="col-12 col-sm-6 col-md-3 d-flex">
+          <div
+            className="card flex-fill"
+            onClick={(e) => {
+              this.handleClick(e);
+            }}
+          >
+            <img src="#" className="card-img-top" alt="digital art"></img>
+            <div className="card-body">
+              <h5 className="card-title">more from the past...</h5>
+              <p className="card-text">Date</p>
+            </div>
+          </div>
+        </div>
+        <div id="past_competitions">{past_competitions}</div>
+      </div>
+    );
+  }
+
   render() {
+    const active_competitions = COMPETITIONS.filter((elem) => {
+      return elem.status === "active";
+    });
+    active_competitions.sort((a, b) => {
+      return new Date(a.date) - new Date(b.date);
+    });
+    const past_competitions = COMPETITIONS.filter((elem) => {
+      return elem.status === "past";
+    });
+    past_competitions.sort((a, b) => {
+      return new Date(a.date) - new Date(b.date);
+    });
+    const future_competitions = COMPETITIONS.filter((elem) => {
+      return elem.status === "comming_soon";
+    });
+    future_competitions.sort((a, b) => {
+      return new Date(a.date) - new Date(b.date);
+    });
+
     return (
       <div className="container mt-5">
         <div className="row">
@@ -44,52 +128,14 @@ class Competitions extends Component {
         </p>
         <div className="row">
           <div className="col-12 col-sm-6 col-md-3 d-flex">
-            <div
-              className="card flex-fill bg-dark"
-              onClick={(e) => {
-                this.handleClick(e);
-              }}
-              onMouseOver={(e) => {
-                this.handleMouseOver(e);
-              }}
-              onMouseLeave={(e) => {
-                this.handleMouseLeave(e);
-              }}
-            >
-              <img src="#" className="card-img-top" alt="graphic design"></img>
-              <div className="card-body">
-                <h5 className="card-title">UI Design</h5>
-                <p className="card-text">Date</p>
-              </div>
-            </div>
+            {this.renderCompetition(active_competitions[0])}
+          </div>
+          <div className="col-12 col-sm-6 col-md-3 d-flex">
+            {this.renderCompetition(active_competitions[1])}
           </div>
           <div className="col-12 col-sm-6 col-md-3 d-flex">
             <div
-              className="card flex-fill bg-dark"
-              onClick={(e) => {
-                this.handleClick(e);
-              }}
-              onMouseOver={(e) => {
-                this.handleMouseOver(e);
-              }}
-              onMouseLeave={(e) => {
-                this.handleMouseLeave(e);
-              }}
-            >
-              <img
-                src="#"
-                className="card-img-top"
-                alt="user interface design"
-              ></img>
-              <div className="card-body">
-                <h5 className="card-title">Digital Arts</h5>
-                <p className="card-text">Date</p>
-              </div>
-            </div>
-          </div>
-          <div className="col-12 col-sm-6 col-md-3 d-flex">
-            <div
-              className="card flex-fill bg-dark"
+              className="card flex-fill"
               onClick={(e) => {
                 this.handleClick(e);
               }}
@@ -103,7 +149,7 @@ class Competitions extends Component {
           </div>
           <div className="col-12 col-sm-6 col-md-3 d-flex">
             <div
-              className="card flex-fill bg-dark"
+              className="card flex-fill"
               onClick={(e) => {
                 this.handleClick(e);
               }}
@@ -120,9 +166,9 @@ class Competitions extends Component {
             </div>
           </div>
         </div>
-        <div className="row">
-          {this.state.competition_type ? this.state.competition_type : null}
-        </div>
+        {this.state.competition_type
+          ? this.renderPastCompetitions(past_competitions)
+          : null}
       </div>
     );
   }
